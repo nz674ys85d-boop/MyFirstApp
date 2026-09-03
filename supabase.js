c8837d0678c74292f41412fd6ed481659010c2a9
nz674ys85d-boop <nz674ys85d@privaterelay.appleid.com>
2026-09-03 17:12:20 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://vgnjlseucwmsfatmikms.supabase.coPublishable key";
+const SUPABASE_URL = "https://vgnjlseucwmsfatmikms.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_p8DkYPSFXTXQnCHqnNdggQ_Blv_HcqR";
 
 const mySupabase = window.supabase.createClient(
